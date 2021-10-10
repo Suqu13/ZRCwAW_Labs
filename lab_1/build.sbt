be528c3e69083bc87b6ab8571c2eece543e3d300
bartosz.figiel <bfigiel@networkedassets.org>
2021-10-10 19:34:42 +0200

    Add CE-3, for resource safety :)

--- lab_1/build.sbt
+++ lab_1/build.sbt
@@ -4,5 +4,16 @@ version := "0.1"
 
 scalaVersion := "2.13.6"
 
-libraryDependencies += "com.amazonaws" % "aws-java-sdk" % "1.12.80"
+addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
 
+libraryDependencies ++= Seq(
+  "com.amazonaws" % "aws-java-sdk" % "1.12.80",
+  "org.typelevel" %% "cats-effect" % "3.2.9" withSources() withJavadoc(),
+)
+
+scalacOptions ++= Seq(
+  "-feature",
+  "-deprecation",
+  "-unchecked",
+  "-language:postfixOps"
+)
--- lab_1/src/main/scala/S3Download.scala
+++ lab_1/src/main/scala/S3Download.scala
@@ -1,32 +1,67 @@
+import cats.effect.std.Console
+import cats.effect.{ExitCode, IO, IOApp, Resource, Sync}
 import com.amazonaws.regions.Regions
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import cats.syntax.all._
+import com.amazonaws.services.s3.model.S3Object
 
 import java.io.{File, FileOutputStream, InputStream, OutputStream}
-import scala.annotation.tailrec
 
-object S3Download extends App {
-  val s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build()
+object S3Download extends IOApp {
   val bucketName = "413742098112-static-website-bucket"
   val objectKey = "index.html"
-  val s3Object = s3.getObject(bucketName, objectKey)
-  val inputStream = s3Object.getObjectContent
-  val outputStream = new FileOutputStream(new File(objectKey))
-  val buffer: Array[Byte] = new Array[Byte](1024 * 10)
 
-  val bytes = read(inputStream, outputStream, buffer, 0)
-  println(s"Read $bytes from $bucketName:$objectKey")
-
-  @tailrec
-  def read(inputStream: InputStream, outputStream: OutputStream, buff: Array[Byte], acc: Long): Long = {
-    val readBytes = inputStream.read(buff, 0, buff.length)
-    if (readBytes > -1) {
-      outputStream.write(buff, 0, readBytes)
-      read(inputStream, outputStream, buff, acc + readBytes)
-    } else {
-      inputStream.close()
-      outputStream.close()
-      acc
+  override def run(args: List[String]): IO[ExitCode] = for {
+    _ <- s3Client[IO].use { client =>
+      for {
+        s3Object <- objectRequest[IO](client, bucketName, objectKey)
+        dest = new File(objectKey)
+        bytes <- download[IO](s3Object, dest)
+        _ <- IO.println(s"Downloaded $bytes bytes from $bucketName/$objectKey")
+      } yield ()
     }
-  }
+  } yield ExitCode.Success
+
+
+  private def download[F[_]: Sync: Console](s3Object: S3Object, destination: File): F[Long] =
+    streams(s3Object, destination).use { case (in, out) => transmit(in, out, new Array[Byte](1024 * 10), 0L) }
+
+  private def s3Client[F[_]: Sync: Console]: Resource[F, AmazonS3] =
+    Resource.make {
+      Sync[F].blocking(AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build())
+    }{ client =>
+      Sync[F].blocking(client.shutdown()).handleErrorWith(e => Console[F].println(e.getMessage))
+    }
+
+  private def objectRequest[F[_]: Sync: Console](client: AmazonS3, bucketName: String, objectKey: String): F[S3Object] =
+    Sync[F].blocking(client.getObject(bucketName, objectKey))
+
+  private def inputStream[F[_]: Sync : Console](s3Object: S3Object): Resource[F, InputStream] =
+    Resource.make {
+      Sync[F].blocking(s3Object.getObjectContent)
+    } { inStr =>
+      Sync[F].blocking(inStr.close()).handleErrorWith(e => Console[F].println(e.getMessage))
+    }
+
+  private def outputStream[F[_]: Sync : Console](f: File): Resource[F, OutputStream] =
+    Resource.make {
+      Sync[F].blocking(new FileOutputStream(f))
+    } { outStr =>
+      Sync[F].blocking(outStr.close()).handleErrorWith(e => Console[F].println(e.getMessage))
+    }
+
+  private def streams[F[_]: Sync : Console](s3Object: S3Object, destination: File): Resource[F, (InputStream, OutputStream)] =
+    for {
+      inStr <- inputStream(s3Object)
+      outStr <- outputStream(destination)
+    } yield (inStr, outStr)
+
+
+  private def transmit[F[_]: Sync](origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): F[Long] =
+    for {
+      amount <- Sync[F].blocking(origin.read(buffer, 0, buffer.length))
+      count  <- if(amount > -1) Sync[F].blocking(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
+      else Sync[F].pure(acc)
+    } yield count
 
 }
