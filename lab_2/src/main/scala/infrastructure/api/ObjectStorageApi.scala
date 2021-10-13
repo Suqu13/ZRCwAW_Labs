6b2e73c8238ecd795e9351d4c93314ae90b12e1e
Bartosz Figiel <242398@student.pwr.edu.pl>
2021-10-13 21:11:16 +0200

    Fix path matching for object key

--- lab_2/src/main/scala/infrastructure/api/ObjectStorageApi.scala
+++ lab_2/src/main/scala/infrastructure/api/ObjectStorageApi.scala
@@ -7,7 +7,6 @@ import io.circe.generic.auto._
 import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.dsl.io._
-import service.S3ObjectStorageService
 import service.spi.ObjectStorageService
 
 class ObjectStorageApi(objectStorageService: ObjectStorageService[IO]) extends HttpApi[IO] {
@@ -20,8 +19,8 @@ class ObjectStorageApi(objectStorageService: ObjectStorageService[IO]) extends H
         e => NotFound(e.getMessage),
         Ok(_)
       )
-    case GET -> Root / "s3" / storageName / objectKey =>
-      objectStorageService.downloadObject(storageName, objectKey).foldF(
+    case GET -> "s3" /: storageName /: key =>
+      objectStorageService.downloadObject(storageName, key.toString()).foldF(
         e => NotFound(e.getMessage),
         r => Ok(readInputStream(IO(r), 1024))
       )
