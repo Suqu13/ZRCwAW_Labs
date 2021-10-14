60295d481d67071d962db100f6d2e2d43f1142ff
Bartosz Figiel <242398@student.pwr.edu.pl>
2021-10-14 20:08:30 +0200

    init forntend

---
+++ lab_2/frontend/src/pages/BucketList.tsx
@@ -0,0 +1,10 @@
+import { List } from "material-ui";
+import React, { FunctionComponent } from "react";
+
+const BucketList: FunctionComponent<undefined> = () => {
+
+  return (
+    <List>
+    </List>
+  )
+}
